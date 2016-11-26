2a025ecdda504c025285387b75d6df59e63bfb2f
tecknork <tecknork@DESKTOP-LUEQSUC>
2016-11-27 01:01:51 +0300

    Add project files.

---
+++ DataLayer/DataLayerModels/MafiaUser.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DataLayerModels
+{
+  public class MafiaUser
+    {
+        [BsonElement("_id")]
+        public int mafiaUserId { get; set; }
+
+        public string userName { get; set; }
+
+        public string role { get; set; }
+
+
+    }
+}
